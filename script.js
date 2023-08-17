67a9331ed6889a167d2f56a65218c1f6713b39d2
Fiona Wong <wong0328@algonquinlive.com>
2023-08-17 12:21:04 -0400

    added interactive gallery components

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ console.log(document.getElementById('date-input'))
 document.getElementById('date-input').max=currentDate
 
 
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const img = document.getElementById("imageone");
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+const images = document.getElementById("photosofthedayfavouritespage")
+images.addEventListener('click', function (e) {
+   console.log(e.target)
+  modal.style.display = "block";
+    modalImg.src = "photosofthedayfavouritespage" + e.target.id + ".png" ; 
+    captionText.innerHTML = e.target.alt;
+  })
+
+
+
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
