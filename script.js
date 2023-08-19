cf66f805cb04e582adfd30e785e1dc0995640acb
Fiona Wong <wong0328@algonquinlive.com>
2023-08-18 23:26:09 -0400

    added the buttons

--- script.js
+++ script.js
@@ -7,35 +7,6 @@ console.log(currentDate, currentDate);
 
 dateInput.max = currentDate;
 
-/*
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-const img = document.getElementById("imageone");
-const modalImg = document.getElementById("img01");
-const captionText = document.getElementById("caption");
-const images = document.getElementById("images")
-images.addEventListener('click', function (e) {
-   console.log(e.target)
-  modal.style.display = "block";
-    modalImg.src = "images/" + e.target.id + ".png" ; 
-    captionText.innerHTML = e.target.alt;
-  })
-
-
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-
-
-*/
-
-
-
 
 dateInput.addEventListener('change', function (e) {
     
@@ -64,6 +35,12 @@ function getNASAData(query_date) {
 
 
 
+
+
+
+// -------------------------seperating 
+
+
 let favouritePhotos = [];
 
 
@@ -87,4 +64,3 @@ function loadFavouritesFromLocalStorage() {
     }
 }
 
-  
